a376901b16ad7cfd0fc56d6a7b6dcf9ec8c7603c
Thiago <thiagohcarvalho81@hotmail.com>
2020-06-07 23:28:29 -0300

    The Client, Order, OrderItem, Product and OrderStatus (enum) classes were created

---
+++ src/entities/Order.java
@@ -0,0 +1,18 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> orderItem = new ArrayList<>();
+		
+	
+}
